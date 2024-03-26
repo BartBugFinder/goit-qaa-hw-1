f51c5ca58355ca14cc20a0b30776517e88ae7cfd
BartBugFinder <153677758+BartBugFinder@users.noreply.github.com>
2024-03-26 14:30:46 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Zadanie 1 
+
+
+// const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+//Zadanie 2 
+
+// const numbers = [2, 5, 8, 3, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 5) {
+//         console.log(numbers[i]);
+//     }
+// }
